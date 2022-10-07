edb85e98a8251b7084903ef5bef823c4ccf5f58d
PASINDUMALEESHA <98903563+PASINDUMALEESHA@users.noreply.github.com>
2022-10-07 15:43:10 +0530

    Add files via upload

---
+++ clientsignup/build/generated/src/org/apache/jsp/registration_jsp.java
@@ -0,0 +1,94 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class registration_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html>\n");
+      out.write("     <head>\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("        <title>Register Account</title>\n");
+      out.write("       <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">\n");
+      out.write("    <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css\" integrity=\"sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh\" crossorigin=\"anonymous\">\n");
+      out.write("        <script src=\"https://code.jquery.com/jquery-3.4.1.slim.min.js\" integrity=\"sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n\" crossorigin=\"anonymous\"></script>\n");
+      out.write("<script src=\"https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js\" integrity=\"sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo\" crossorigin=\"anonymous\"></script>\n");
+      out.write("<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js\" integrity=\"sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6\" crossorigin=\"anonymous\"></script>\n");
+      out.write("\n");
+      out.write("    </head>\n");
+      out.write("    <body>\n");
+      out.write("        <div class=\"container\">\n");
+      out.write("            <div class=\"regbox box\">\n");
+      out.write("                \n");
+      out.write("                <h1><center>Register Account</center></h1>\n");
+      out.write("                <center>\n");
+      out.write("<form action=\"registation\" method=\"post\">\n");
+      out.write("                   <p>NICno</p>\n");
+      out.write("<input type=\"text\" placeholder=\"NIC no\" name=\"nicno\" required>\n");
+      out.write("                   <p>email</p>\n");
+      out.write("<input type=\"text\" placeholder=\"email\" name=\"email\" required>\n");
+      out.write("                   <p>Username</p>\n");
+      out.write("<input type=\"text\" placeholder=\"Username\" name=\"name\" required>\n");
+      out.write("                   <p>Password</p>\n");
+      out.write("                   <input type=\"password\" placeholder=\"Password\" name=\"password\" required>\n");
+      out.write("                   <br><br><br>\n");
+      out.write("                   <button type=\"submit\" class=\"btn btn-success\">Register</button><br><br><button type=\"reset\" class=\"btn btn-success\" >RESET</button>\n");
+      out.write("                   <br/><br/>\n");
+      out.write("                   <a href=\"index.jsp\">Already have Account?</a><br/><br/>\n");
+      out.write("                   <a href=\"http://localhost:8080/clientsignup/registration.jsp\"> staff members registration</a>\n");
+      out.write("                </form></center>\n");
+      out.write("</div>\n");
+      out.write("</div>\n");
+      out.write("</body>\n");
+      out.write("</html>");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
---
+++ clientsignup/build/web/registration.jsp
@@ -0,0 +1,38 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+     <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Register Account</title>
+       <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    </head>
+    <body>
+        <div class="container">
+            <div class="regbox box">
+                
+                <h1><center>Register Account</center></h1>
+                <center>
+<form action="registation" method="post">
+                   <p>NICno</p>
+<input type="text" placeholder="NIC no" name="nicno" required>
+                   <p>email</p>
+<input type="text" placeholder="email" name="email" required>
+                   <p>Username</p>
+<input type="text" placeholder="Username" name="name" required>
+                   <p>Password</p>
+                   <input type="password" placeholder="Password" name="password" required>
+                   <br><br><br>
+                   <button type="submit" class="btn btn-success">Register</button><br><br><button type="reset" class="btn btn-success" >RESET</button>
+                   <br/><br/>
+                   <a href="index.jsp">Already have Account?</a><br/><br/>
+                   <a href="http://localhost:8080/clientsignup/registration.jsp"> staff members registration</a>
+                </form></center>
+</div>
+</div>
+</body>
+</html>
